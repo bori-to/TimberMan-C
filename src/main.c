a815aa34f186bc32cc95f615e7873b8544c9acc6
Adrien <adrien0878@gmail.com>
2024-01-19 16:35:47 +0100

    Ajout highScore

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
 /*
     GNU/Linux et MacOS
-        > gcc src/main.c -o prog $(sdl2-config --cflags --libs)
+        > gcc src/*.c -o bin/prog $(sdl2-config --cflags --libs)
     Windows
-        > gcc src/main.c -o prog -I include -L lib -lmingw32 -lSDL2main -lSDL2
+        > gcc src/*.c -o bin/prog -I include -L lib -lmingw32 -lSDL2main -lSDL2
 */
 
 #include <stdio.h>
